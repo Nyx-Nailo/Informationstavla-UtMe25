29eeaccbdd29c202a79b1ba93b9b54874eb853f6
anoh <33670143+anohr@users.noreply.github.com>
2022-12-16 23:25:11 +0100

    upd { small changes }

--- tuc-hotel/src/pages/system/system.js
+++ tuc-hotel/src/pages/system/system.js
@@ -7,61 +7,55 @@ import Nyheter from '../nyheter/nyheter';
 import Vader from '../vader/vader';
 import Busstider from '../busstider/busstider';
 
-
-
 const System = () => {
-    
-    /*  Clock   */
-    const [time, setTime] = useState(new Date());
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setTime(new Date());
-        }, 1000);
-        return () => clearInterval(interval);
-    }, [])
-    const Clock = () => {
-        return (
-            <>
-                {time.toLocaleDateString("sv")} {time.toLocaleTimeString("sv")}
-            </>
-            );
-    }
+  const [time, setTime] = useState(new Date());
+  const [pageNr, setPageNr] = useState(0);
+  const [delay, setDelay] = useState(0);
+  const [page, setPage] = useState(<>Loading</>);
 
-    /*  Timer for page change   */
-    const pages = [<Dagsschema />, <Lunch />, <Nyheter />, <Vader />, <Busstider />];
-    const viewTime = [5, 3, 3, 3, 3]; /*Tid i sekunder som sidan med samma index visas*/
-    const [pageNr, setPageNr] = useState(0);
-    const [delay, setDelay] = useState(0);
-    const [page, setPage] = useState(<>Loading</>)
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setPage(pages[pageNr]);
-            setDelay(viewTime[pageNr]* 1000);
-            if (pageNr === pages.length - 1) {
-                setPageNr(0); 
-            } else {
-                setPageNr((prevPageNr) => prevPageNr + 1);
-            }
-        }, delay);
-        return () => clearInterval(interval);
-    }, [pageNr, delay]);
-
-
+  const Clock = () => {
     return (
-        <div className='container-fluid'>
-            <div className="row">
-                <div className="col-10"></div>
-                <div className="col-1">
-                    <Clock />
-                </div>
-            </div>
-            <div className='row'>
-                <div className='col'></div>
-                <div className='col'>{page}</div>
-                <div className='col'></div>
-            </div>
+      <div className='position-absolute top-0 end-0 text-center text-bg-dark col shadow-sm'>
+        <div className='p-4'>
+          <h5 className='m-0'>{time.toLocaleDateString('sv')}</h5>
+          <h2>
+            {time.getHours()}:{time.getMinutes()}
+          </h2>
         </div>
+      </div>
     );
+  };
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  useEffect(() => {
+    const pages = [<Dagsschema />, <Lunch />, <Nyheter />, <Vader />, <Busstider />];
+    /*  Timer for page change   */
+    const viewTime = [2, 1, 1, 1, 1]; /*Tid i sekunder som sidan med samma index visas*/
+
+    const interval = setInterval(() => {
+      setPage(pages[pageNr]);
+      setDelay(viewTime[pageNr] * 5000);
+      if (pageNr === pages.length - 1) {
+        setPageNr(0);
+      } else {
+        setPageNr((prevPageNr) => prevPageNr + 1);
+      }
+    }, delay);
+    return () => clearInterval(interval);
+  }, [pageNr, delay]);
+
+  return (
+    <>
+      <Clock />
+      {page}
+    </>
+  );
 };
 
 export default System;
