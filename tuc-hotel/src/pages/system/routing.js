ed74258a4ea61bf16e1ba44bcac34048677cc5ac
anoh <33670143+anohr@users.noreply.github.com>
2022-12-18 13:45:25 +0100

    feat { added routing to static page }

--- tuc-hotel/src/pages/system/routing.js
+++ tuc-hotel/src/pages/system/routing.js
@@ -5,16 +5,27 @@ import System from './system';
 import Admin from './admin';
 import LogIn from './logIn';
 
+import Dagsschema from '../dagsschema/dagsschema';
+import Lunch from '../lunch/lunch';
+import Nyheter from '../nyheter/nyheter';
+import Vader from '../vader/vader';
+import Busstider from '../busstider/busstider';
+
 const routing = () => {
-	return (
-        <Router>
-            <Routes>
-                <Route path='/' element={<LogIn />} />
-                <Route path='/admin' element={<Admin />} />
-                <Route path='/slideShow' element={<System />} />
-            </Routes>
-        </Router>
-		);
-}
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<LogIn />} />
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/slideShow' element={<System />} />
+        <Route path='/Dagsschema' element={<Dagsschema />} />
+        <Route path='/Lunch' element={<Lunch />} />
+        <Route path='/Nyheter' element={<Nyheter />} />
+        <Route path='/Vader' element={<Vader />} />
+        <Route path='/Busstider' element={<Busstider />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default routing;
