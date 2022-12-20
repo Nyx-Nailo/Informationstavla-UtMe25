30e11d4595754dfec84154f2c27e782a725cce59
anoh <33670143+anohr@users.noreply.github.com>
2022-12-20 09:26:18 +0100

    fix { vader images }

--- tuc-hotel/src/pages/vader/vader.js
+++ tuc-hotel/src/pages/vader/vader.js
@@ -2,37 +2,117 @@ import React, { useEffect, useState } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const url =
-  'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/15.596/lat/58.4343/data.json';
+const url = 'https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/15.596/lat/58.4343/data.json';
 
 const symbols = [
-  'Klar himmel',
-  'Nästan klar himmel',
-  'Blandade moln',
-  'Halv klar himmel',
-  'Molnig himmel',
-  'Mulet',
-  'Dimma',
-  'Lätta regnskurar',
-  'Måttliga regnskurar',
-  'Kraftiga regnskurar',
-  'Åskstorm',
-  'Lätta snöblandade regnskurar',
-  'Måttliga snöblandade regnskurar',
-  'Kraftiga snöblandade regnskurar',
-  'Lätta snöskurar',
-  'Måttliga snöskurar',
-  'Kraftiga snöskurar',
-  'Lätt regn',
-  'Måttligt regn',
-  'Kraftigt regn',
-  'Åska',
-  'Lätt snöblandat regn',
-  'Måttligt snöblandat regn',
-  'Kraftigt snöblandat regn',
-  'Lätt snöfall',
-  'Måttligt snöfall',
-  'Kraftigt snöfall',
+  {
+    type: 'Klar himmel',
+    img: '../img/img/vader/sun.svg',
+  },
+  {
+    type: 'Nästan klar himmel',
+    img: '../img/vader/sunsmallcloud.svg',
+  },
+  {
+    type: 'Blandade moln',
+    img: '../img/vader/suncloud.svg',
+  },
+  {
+    type: 'Halv klar himmel',
+    img: '../img/vader/sunsmallcloud.svg',
+  },
+  {
+    type: 'Molnig himmel',
+    img: '../img/vader/cloud.svg',
+  },
+  {
+    type: 'Mulet',
+    img: '../img/vader/cloud.svg',
+  },
+  {
+    type: 'Dimma',
+    img: '../img/vader/fog.svg',
+  },
+  {
+    type: 'Lätta regnskurar',
+    img: '../img/vader/lightrain.svg',
+  },
+  {
+    type: 'Måttliga regnskurar',
+    img: '../img/vader/rain.svg',
+  },
+  {
+    type: 'Kraftiga regnskurar',
+    img: '../img/vader/rain.svg',
+  },
+  {
+    type: 'Åskstorm',
+    img: '../img/vader/lightingrain.svg',
+  },
+  {
+    type: 'Lätta snöblandade regnskurar',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Måttliga snöblandade regnskurar',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Kraftiga snöblandade regnskurar',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Lätta snöskurar',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Måttliga snöskurar',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Kraftiga snöskurar',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Lätt regn',
+    img: '../img/vader/rain.svg',
+  },
+  {
+    type: 'Måttligt regn',
+    img: '../img/vader/rain.svg',
+  },
+  {
+    type: 'Kraftigt regn',
+    img: '../img/vader/rain.svg',
+  },
+  {
+    type: 'Åska',
+    img: '../img/vader/lightning.svg',
+  },
+  {
+    type: 'Lätt snöblandat regn',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Måttligt snöblandat regn',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Kraftigt snöblandat regn',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Lätt snöfall',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Måttligt snöfall',
+    img: '../img/vader/snow.svg',
+  },
+  {
+    type: 'Kraftigt snöfall',
+    img: '../img/vader/snow.svg',
+  },
 ];
 const days = ['Söndag', 'Måndag', 'Tisdag', 'Onsdag', 'Torsdag', 'Fredag', 'Lördag'];
 
@@ -42,8 +122,10 @@ const VaderIDag = (props) => {
   const firstSymbol = props.data[0].weatherSymbol;
 
   return (
-    <div className='text-center position-absolute top-50 start-50 translate-middle'>
-      <div className='card-body'>
+    <div className='text-center position-absolute top-50 start-50 translate-middle container-fluid'>
+      <div
+        className='card-body p-5'
+        style={{ backgroundPosition: 'center', backgroundRepeat: 'no-repeat', backgroundImage: 'linear-gradient(295deg, rgba(255, 255, 255, .5), rgba(255,255,255, 0.25)), url(' + symbols[props.data[0].weatherSymbol].img }}>
         <div>
           <h6 className='p-0 m-0' style={{ fontSize: '1rem' }}>
             {days[new Date().getDay()]}
@@ -52,7 +134,7 @@ const VaderIDag = (props) => {
             {Math.ceil(firstTemp)}°
           </h1>
           <p className='p-0 m-0'>
-            {Math.floor(firstWind)}m/s, {symbols[firstSymbol]}
+            {Math.floor(firstWind)}m/s, {symbols[firstSymbol].type}
           </p>
         </div>
       </div>
@@ -70,13 +152,16 @@ const VaderSjuDagar = (props) => {
             let dayNumber = whatDay.getDay();
 
             return (
-              <div key={index} className='card col m-2 p-3'>
+              <div
+                key={index}
+                className='card col m-2 p-3'
+                style={{ backgroundPosition: '-115%', backgroundRepeat: 'no-repeat', backgroundImage: 'linear-gradient(295deg, rgba(255, 255, 255, 1), rgba(255,255,255, 0.75), rgba(255,255,255, 0.25)), url(' + symbols[data.weatherSymbol].img }}>
                 <div>
                   <h6>{days[dayNumber]}</h6>
                 </div>
                 <div>
                   <h1>{Math.ceil(data.temperature)}°</h1>
-                  {Math.floor(data.wind)}m/s, {symbols[data.weatherSymbol]}
+                  {Math.floor(data.wind)}m/s, {symbols[data.weatherSymbol].type}
                 </div>
               </div>
             );
@@ -102,25 +187,15 @@ const Vader = () => {
   today = yyyy + '-' + mm + '-' + dd;
 
   useEffect(() => {
-    if (
-      typeof sevenDayData === 'undefined' ||
-      typeof todayWeatherData === 'undefined' ||
-      todayWeatherData[0].validTime.slice(0, -10) !== today
-    ) {
+    if (typeof sevenDayData === 'undefined' || typeof todayWeatherData === 'undefined' || todayWeatherData[0].validTime.slice(0, -10) !== today) {
       setIsLoading(true);
       fetch(url)
         .then((response) => response.json())
         .then((data) => {
           const transformedTimeSeries = data.timeSeries.map((timeseries) => {
-            const temperatureParameter = timeseries.parameters.find(
-              (parameter) => parameter.name === 't'
-            );
-            const windParameter = timeseries.parameters.find(
-              (parameter) => parameter.name === 'ws'
-            );
-            const weatherSymbol = timeseries.parameters.find(
-              (parameter) => parameter.name === 'Wsymb2'
-            );
+            const temperatureParameter = timeseries.parameters.find((parameter) => parameter.name === 't');
+            const windParameter = timeseries.parameters.find((parameter) => parameter.name === 'ws');
+            const weatherSymbol = timeseries.parameters.find((parameter) => parameter.name === 'Wsymb2');
             return {
               validTime: timeseries.validTime,
               temperature: temperatureParameter.values,
@@ -136,15 +211,9 @@ const Vader = () => {
           const temperatureData = data.timeSeries
             .filter((timePeriod) => timePeriod.validTime.slice(11, -4) === '12:00')
             .map((timePeriod) => {
-              const temperatureParameter = timePeriod.parameters.find(
-                (parameter) => parameter.name === 't'
-              );
-              const windParameter = timePeriod.parameters.find(
-                (parameter) => parameter.name === 'ws'
-              );
-              const weatherSymbol = timePeriod.parameters.find(
-                (parameter) => parameter.name === 'Wsymb2'
-              );
+              const temperatureParameter = timePeriod.parameters.find((parameter) => parameter.name === 't');
+              const windParameter = timePeriod.parameters.find((parameter) => parameter.name === 'ws');
+              const weatherSymbol = timePeriod.parameters.find((parameter) => parameter.name === 'Wsymb2');
 
               return {
                 time: timePeriod.validTime,
