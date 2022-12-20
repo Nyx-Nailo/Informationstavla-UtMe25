bd10d17e2219c6ddbf3c4c1902912833c9f8182b
Fredrik Anborén <fredrik.anboren.syne22lin@edu.tucsweden.se>
2022-12-20 11:22:59 +0100

    progbar

--- tuc-hotel/src/pages/system/system.js
+++ tuc-hotel/src/pages/system/system.js
@@ -13,6 +13,16 @@ const System = () => {
   const [pageNr, setPageNr] = useState(0);
   const [delay, setDelay] = useState(0);
   const [page, setPage] = useState(<>Loading</>);
+  const [progress, setProgress] = useState(0);
+  const [elapsed, setElapsed] = useState(0);
+
+  const ProgressBar = () => {
+    return (
+      <div className="progress fixed-bottom" style={{ height: "8px" }}>
+        <div className="progress-bar" role="progressbar" aria-label="Basic example" style={{ width: progress + "%" }} aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+      </div>
+    );
+  }
 
   const Clock = () => {
     return (
@@ -36,26 +46,33 @@ const System = () => {
   }, []);
 
     /*  Timer för sidorna   */
+ 
+
   useEffect(() => {
     const pages = [<Dagsschema />, <Lunch />, <Nyheter />, <Vader />, <Busstider />];
     /*behöver en sida mer tid ändras det här, tiderna visas i sekunder*/
-    const viewTime = [2/*Dagsschema*/, 1/*Lunch*/, 1/*Nyheter*/, 10/*Väder*/, 1/*Buss*/];
+    const viewTime = [10/*Dagsschema*/, 10/*Lunch*/, 10/*Nyheter*/, 10/*Väder*/, 10/*Buss*/];
     const interval = setInterval(() => {
       setPage(pages[pageNr]);
-      setDelay(viewTime[pageNr] * 1000);/*1000 behövs för att tiderna i viewTime ska stämma*/
-      if (pageNr === pages.length - 1) {
-        setPageNr(0);
-      } else {
-        setPageNr((prevPageNr) => prevPageNr + 1);
+      setDelay(viewTime[pageNr] * 1000);
+      setElapsed((prevElapsed) => prevElapsed + 0.01);
+      setProgress((elapsed / viewTime[pageNr]) * 100);
+      if (elapsed > viewTime[pageNr]) {
+        if (pageNr === pages.length - 1) {
+          setPageNr(0);
+        } else {
+          setPageNr((prevPageNr) => prevPageNr + 1);
+        }
+        setElapsed(0);
       }
-    }, delay);
-      return () => clearInterval(interval);
-  }, [pageNr, delay]);
-
+    }, 10);
+    return () => clearInterval(interval);
+  }, [elapsed, progress, pageNr]);
 
   return (
     <>
       <Clock />
+      <ProgressBar />
       {page}
     </>
   );
