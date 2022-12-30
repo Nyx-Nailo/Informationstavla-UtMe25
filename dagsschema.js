029961fdf7be613db0754795b87deab458ca8a70
StafaAlasady <116460154+StafaAlasady@users.noreply.github.com>
2022-12-30 20:03:59 +0100

    Add files via upload

---
+++ dagsschema.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Schema from "./Schema.js";
+function dagsschema() {
+  return (
+    <div id='dagsschema'>
+      <Schema/>
+    </div>
+  );
+}
+
+export default dagsschema;
